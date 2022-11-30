b55b3c294cfd8002c15df3f21d0c3f37241b4e0a
D <manhtom2003@gmail.com>
2022-11-30 09:01:29 +0700

    Test MIPS asm for I/O integer & conditionals
    ##input, count, res=0
    if input>2 then {
      res=input*5;
      print(res);
    }
    else {
      res=input*3;
      print(res);
    }

---
+++ mips1.asm
@@ -0,0 +1,23 @@
+
+li $v0, 5
+syscall
+add $t0, $zero, $v0 #move input from v0 to t0
+bgt $t0, 2, multi2
+
+# t0: input, t1: keep count, $s0, res
+multi1:
+addi $s0, $s0, 3
+addi $t1, $t1, 1
+beq $t1, $t0, exit
+j multi1
+
+multi2:
+addi $s0, $s0, 5
+addi $t1, $t1, 1
+beq $t1, $t0, exit
+j multi2
+
+exit:
+li $v0, 1
+la $a0, ($s0)
+syscall
